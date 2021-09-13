f0f578aa450b04c59949ca0de6951fdb898da9e9
dylan-pecqueux <dylan.pecqueux34@outlook.fr>
2021-09-13 18:42:57 +0200

    cleab db

--- bookapp/models.py
+++ bookapp/models.py
@@ -5,7 +5,7 @@ from django.conf import settings
 
 class Ticket(models.Model):
     title = models.CharField(max_length=128)
-    description = models.TextField(max_length=2048)
+    description = models.TextField(max_length=2048, blank=True)
     user = models.ForeignKey(
         to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     image = models.ImageField(upload_to='images', null=True, blank=True)
